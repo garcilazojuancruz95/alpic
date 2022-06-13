77cf574b8f301825f5afdab9211c1478ef163cb9
Juan Cruz Garcilazo <garcilazo.juancruz@hotmail.com>
2022-06-13 18:58:24 -0300

    usuario estilo

--- views/modules/pages/crearUsuarios.views.php
+++ views/modules/pages/crearUsuarios.views.php
@@ -1,3 +1,30 @@
-<main>
+<main id="crearUsuario">
 	<!-- el contenido de la pagina  -->
+	
+	<div class="crearUsuario">
+		<h1>Crear Usuarios</h1>
+		<form>
+			<label for="nombre">Nombre completo</label>
+			<input type="text">
+			
+			<label for="email">Email</label>
+			<input type="email">
+
+			<label for="direccion">Direccion</label>
+			<input type="text">
+
+			<label for="telefono">Telefono</label>
+			<input type="number">
+
+			<label for="fecha">Fecha de nacimiento</label>
+			<input type="date">
+
+			<label for="dni">DNI</label>
+			<input type="number">
+
+			<input type="submit" value="CARGAR">
+
+		</form>
+	</div>
+	
 </main>
